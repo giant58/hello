4e1014a0fac0e30967d6923ea372b23e613d89e9
Giant Su <73266231+giant58@users.noreply.github.com>
2020-11-27 20:26:37 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/hello')
+@app.route('/')
 def hello_world():
     name = os.environ.get('NAME', 'World') # if 'NAME' doesn't exist, default is 'World'
     return 'Hello {}!'.format(name)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host='0.0.0.0', port=8080)
